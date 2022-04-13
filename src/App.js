ad008991ad7cd46f11864194cb019f6f492960f1
yoonseayoung <yoondinosour@naver.com>
2022-04-13 12:02:33 +0900

    03 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+/* function Fruit(props) {
+  console.log(props);
+  return (
+    <h1>{props.fav} 맛있어요</h1>
+  )
+} */
+
+function Fruit({fav}) {
+  return <h1> {fav} 맛있어요</h1>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>안녕하세요</h1>
+      <Fruit fav='바나나' />
+      <Fruit fav='망고' />
+      <Fruit fav='수박' />
+      <Fruit fav='딸기' />
+
+
     </div>
   );
 }
---
+++ src/Orange.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Orange() {
+    return <h3>오렌지 맛있어</h3>
+}
+
+export default Orange;
