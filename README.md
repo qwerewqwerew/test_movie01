e153eb31285fd5bb57be84df561976e8a43617a7
qwerewqwerew <qwerew0@gmail.com>
2022-04-14 09:15:07 +0900

    lesson 03

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Movie App 2022
 
 React JS Fundamentals Course 2022
+https://github.com/qwerewqwerew/test_movie01
--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Fruit({ name, picture }) {
+	return (
+		<div>
+			<h2> 내가 좋아하는 {name}</h2>
+			<img src={picture} />
+		</div>
+	);
+}
+const fruitILike = [
+	{
+		id: 1,
+		name: "바나나",
+		image: "http://qwerew.cafe24.com/images/banana.png",
+	},
+	{
+		id: 2,
+		name: "오렌지",
+		image: "http://qwerew.cafe24.com/images/orange.png",
+	},
+	{
+		id: 3,
+		name: "사과",
+		image: "http://qwerew.cafe24.com/images/apple.png",
+	},
+	{
+		id: 4,
+		name: "메론",
+		image: "http://qwerew.cafe24.com/images/melon.jpg",
+	},
+];
+
+const renderFruit = dish => <Fruit key={dish.id} name={dish.name} picture={dish.image} />;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(fruitILike.map(renderFruit));
+	return <div>{fruitILike.map(renderFruit)}</div>;
 }
 
 export default App;
