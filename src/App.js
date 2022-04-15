9444a29e49b2697da9e8e527baa4919115605921
yoonseayoung <yoondinosour@naver.com>
2022-04-15 11:01:01 +0900

    05 리액트 state,class,생명주기 학습

--- src/App.js
+++ src/App.js
@@ -1,66 +1,39 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-/* function Fruit(props) {
-  console.log(props);
-  return (
-    <h1>{props.fav} 맛있어요</h1>
-  )
-} */
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    console.log('constructor 실행완료');
+  }
 
-function Fruit({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>{name} 맛있어요</h2>
-      <h4>{rating} / 5.0 </h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+  state = { count: 0 };
+  add = () => {
+    this.setState({count: this.state.count+1})
+  };
+  minus = () => {
+    this.setState({count: this.state.count-1})
+  };
 
-const fruitILike = [
-  {
-    id:1,
-    name: "바나나",
-    image: "http://qwerew.cafe24.com/images/banana.png",
-    rating: 5,
-  },
-  {
-    id:2,
-    name: "오렌지",
-    image: "http://qwerew.cafe24.com/images/orange.png",
-    rating: 4.9,
-  },
-  {
-    id:3,
-    name: "사과",
-    image: "http://qwerew.cafe24.com/images/apple.png",
-    rating: 3,
-  },
-  {
-    id:4,
-    name: "메론",
-    image: "http://qwerew.cafe24.com/images/melon.jpg",
-    rating: 4.8,
-  },
-];
+  componentDidMount(){
+    console.log('componentDidMount실완');
+  }
+  componentDidUpdate(){
+    console.log('Update실완');
+  }
+  componentWillUnmount(){
+    console.log('비번뭐야');
+  }
 
-/* function renderFruit(dish){
-  return <Fruit key={dish.id} name={dish.name} picture={dish.image} />
-} */
-
-function App() {
-  return (
-    <div>
-      {fruitILike.map(dish=><Fruit key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
-    </div>
-  );
-}
-
-Fruit.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  render() {
+    console.log('render실완');
+    return (
+      <div>
+        <h1>번호는 {this.state.count}</h1>
+        <button onClick={this.add}>더하기</button>
+        <button onClick={this.minus}>빼기</button>
+      </div>
+    );
+  }
 }
 
 export default App;
