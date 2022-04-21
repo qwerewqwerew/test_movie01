78a5a55e0468be70bda68366758cb995c305ad1e
qwerewqwerew <qwerew0@gmail.com>
2022-04-21 23:04:32 +0900

    realy final lesson09 code

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,36 @@
 import React from 'react';
-
+import './Detail.css';
+  
 class Detail extends React.Component {
     componentDidMount() {
-        const { location, history } = this.props; 
-        if (location.state === undefined) { 
+        const { location, history } = this.props;
+        if (location.state === undefined) {
             history.push('/');
-        }
+        } 
     }
-
+  
     render() {
         const { location } = this.props;
         if (location.state) {
-            return <span>{location.state.title}</span>;
+            return (
+                <div className="detail">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                    <div className="movie__data">
+                        <h3 className="movie__title">{location.state.title}</h3>
+                        <h5 className="movie__year">{location.state.year}</h5>
+                        <ul className="movie__genres">
+                            {location.state.genres.map((genre, index) => {
+                                return <li key={location.state.index} className="movie_genre">{location.state.genre}</li>;
+                            })}
+                        </ul> 
+                        <p className="movie__summary">{location.state.summary}</p>
+                    </div>
+                </div> 
+            ); 
         } else {
             return null;
         }
     }
 }
-
+  
 export default Detail;
