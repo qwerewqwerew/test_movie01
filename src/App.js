8f84a12d2836b6fa38103b2111a688b421923831
yoonseayoung <yoondinosour@naver.com>
2022-04-22 11:13:54 +0900

    리액트 영화앱 1차 완성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import axios from "axios"
 import {HashRouter, Route} from "react-router-dom";
+
 import About from "./routes/About"
 import Home from "./routes/Home"
+import Detail from "./routes/Detail"
+import Navigation from "./components/Navigation";
 
 import "./App.css"
-import Navigation from "./components/Navigation";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home}/>
       <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail}/>
     </HashRouter>
   )
 }
