91a56ab85fbb18f66c472656a81eaaf22d33c7b3
yoonseayoung <yoondinosour@naver.com>
2022-04-22 11:59:29 +0900

    리액트 영화앱 최종완성

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css"
 
 class Detail extends React.Component{
     componentDidMount(){
@@ -11,8 +12,18 @@ class Detail extends React.Component{
         const{ location }=this.props;
         if(location.state){
             return (
-                <div>
-                    <span>{location.state.title}</span>
+                <div className="detail">
+                    <img src={location.state.poster} alt={location.state.poster} title={location.state.poster} />
+                    <div className="movie__data">
+                        <h3 className="movie__title">{location.state.title}</h3>
+                        <h5 className="movie__year">{location.state.year}</h5>
+                        <ul className="movie__genres">
+                            {location.state.genres.map((genre,index)=>{
+                                return <li key={index} className="movie_genre">{genre}</li>
+                            })}
+                        </ul>
+                        <p className="movie__summary">{location.state.summary}</p>
+                    </div>
                 </div>
             )
         }else{
